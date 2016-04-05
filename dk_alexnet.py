573e85c9f038e8b09b25428db08ddf0b91bd274d
David Krueger <davidscottkrueger@gmail.com>
2016-04-05 14:20:14 -0400

    alexnet runs

--- dk_alexnet.py
+++ dk_alexnet.py
@@ -28,15 +28,20 @@ print "settings_str=", settings_str
 """
 Pylearn2 wrapper on cudaconvnet
 
-The shapes:
-inputs: input_groups, channels_per_group, nrows, ncols, batch_size
-filters: nfilters_per_row, nfilters_per_col, nchannels_per_filter, nrows_per_filter, ncolumns_per_filter, filter_groups, nfilters_per_group
-biases: nfilters_per_row, nfilters_per_col, filter_groups, nfilters_per_group
-outputs: groups, nfilters_per_group, nfilters_per_row, nfilters_per_column, batch_size
+WIP
 
+for the moment, we assume that filter_groups == 1 always.
 
-TODO: MUST PERFORM PADDING MANUALLY!
 """
+
+shapes_str = """
+The shapes:
+filters: nfilters_per_row, nfilters_per_col, nchannels_per_filter, nrows_per_filter, ncolumns_per_filter, filter_groups, nfilters_per_group
+biases: filter_groups, nfilters_per_group, nfilters_per_row, nfilters_per_col
+inputs: input_groups, channels_per_group, nrows, ncols, batch_size
+outputs: groups, nfilters_per_group, nfilters_per_row, nfilters_per_column, batch_size
+"""
+
 from theano.tensor.signal.pool import pool_2d, Pool
 from pylearn2.packaged_dependencies.theano_linear.unshared_conv.unshared_conv import FilterActs
 locally_connected = FilterActs(1)
@@ -48,24 +53,27 @@ def infer_shapes(activation_shape, filter_sizes, nchannels, pool_sizes, pads):
     biases_shapes = []
     activation_shapes = [activation_shape]
     for sz, chan, pool, pad in zip(filter_sizes, nchannels, pool_sizes, pads):
-        activation_shapes[-1] = [activation_shapes[-1][n] + (0, 0, 2*pad, 2*pad, 0)[n] for n in range(5)] # add padding
+        weights_shapes.append([activation_shape[2],
+                     activation_shape[3],
+                     activation_shape[1],
+                     sz,
+                     sz,
+                     1,
+                     chan])
+        biases_shapes.append([1,
+                              chan,
+                              activation_shape[2],
+                              activation_shape[3]])
+        # add padding
+        activation_shapes[-1] = [activation_shapes[-1][n] + (0, 0, 2*pad, 2*pad, 0)[n] for n in range(5)]
         activation_shape = activation_shapes[-1]
+        # infer next shape (post-pooling)
         activation_shape = [1,
                             chan,
                             (activation_shape[2] - sz + 1) / pool,
                             (activation_shape[3] - sz + 1) / pool,
                             activation_shape[4]]
         activation_shapes.append(activation_shape)
-        weights_shapes.append([activation_shape[2],
-                     activation_shape[3],
-                     activation_shape[4],
-                     sz,
-                     sz,
-                     1,
-                     chan])
-        biases_shapes.append([activation_shape[2],
-                     activation_shape[3],
-                     chan])
     return weights_shapes, biases_shapes, activation_shapes
 
 # replace local params with their average (can be used to enforce CNN weight/bias sharing)
@@ -137,14 +145,16 @@ pads = [2,2,2] # TODO!
 #pads = [0,0,0]
 
 # Make Params
-weights_shapes, biases_shapes, activation_shapes = infer_shapes(activation_shape, filter_sizes, nchannels, pool_sizes, pads)
-print "weights_shapes, biases_shapes, activation_shapes =", weights_shapes, biases_shapes, activation_shapes
+weights_shapes, biases_shapes, activations_shapes = infer_shapes(activation_shape, filter_sizes, nchannels, pool_sizes, pads)
+print shapes_str
+print "weights_shapes =", weights_shapes
+print "biases_shapes =", biases_shapes
+print "activations_shapes =", activations_shapes
 weights = [theano.shared(np.random.uniform(-.01, .01, shp).astype("float32"), name='w'+str(n)) for 
               n,shp in enumerate(weights_shapes)]
 biases = [theano.shared(np.zeros(shp).astype("float32"), name='b'+str(n)) for 
               n,shp in enumerate(biases_shapes)]
-last_layer_dims = [weights[-1].shape.eval()[n] for n in [0,1,-1]]
-output_weight = theano.shared(np.random.uniform(-.01, .01, (np.prod(last_layer_dims), 10)).astype("float32"), 'w_out')
+output_weight = theano.shared(np.random.uniform(-.01, .01, (np.prod(activations_shapes[-1]) / batchsize, 10)).astype("float32"), 'w_out')
 output_bias = theano.shared(np.zeros(10).astype("float32"), 'b_out')
 params = weights + biases + [output_weight, output_bias]
 
@@ -156,15 +166,19 @@ truth.tag.test_value = train_y[:batchsize].eval()
 targets = truth
 #inputs: input_groups, channels_per_group, nrows, ncols, batch_size
 activations = [varin.reshape((1, 3, 32, 32, batchsize))]
-for weight, bias, pool_size, activation_shape in zip(weights, biases, pool_sizes, activation_shapes): # TODO: padding
-    activations[-1] = T.set_subtensor(T.zeros(activation_shape)[:,:,pool_size:-pool_size], activations[-1])
-    preactivations = locally_connected(activations[-1], weight) + bias
+for weight, bias, pool_size, activation_shape in zip(weights, biases, pool_sizes, activations_shapes):
+    # pad with zeros
+    activations[-1] = T.set_subtensor(
+                        T.zeros(activation_shape)[:, :, pool_size:-pool_size, pool_size:-pool_size, :],
+                        activations[-1])
+    preactivations = locally_connected(activations[-1], weight) + bias.dimshuffle(0, 1, 2, 3, 'x')
     activations.append(preactivations * (preactivations > 0))
-    activations.append(pool_2d(activations[-1],
+    # pool_2d pools over the last 2 dims
+    activations.append(pool_2d(activations[-1].dimshuffle(0,1,4,2,3),
                                (pool_size, pool_size),
                                #st=pool_size,
                                mode='max', padding=(0,0),
-                               ignore_border=1))
+                               ignore_border=1).dimshuffle(0,1,3,4,2))
 preoutputs = T.dot(activations[-1].reshape((batchsize, -1)), output_weight) + output_bias
 outputs = T.nnet.softmax(preoutputs)
 
@@ -236,7 +250,9 @@ for step in xrange(finetune_epc * 50000 / batchsize):
     # sharify
     if step % sharify_every_n_batches == 0:
         print "sharifying weights..."
-        [sharify(W) for W in weights]
+        [sharify(W, range(2), range(2, 7)) for W in weights]
+        print "sharifying biases..."
+        [sharify(b, range(2,4), range(2)) for b in biases]
         print "Done."
 
     if step % (50000 / batchsize) == 0 and step > 0:
@@ -246,7 +262,7 @@ for step in xrange(finetune_epc * 50000 / batchsize):
         crnt_avg[ind] = epc_cost
         if sum(hist_avg) < sum(crnt_avg):
             break
-    
+ 
         # adjust learning rate
         if prev_cost <= epc_cost:
             patience += 1
