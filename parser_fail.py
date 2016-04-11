a7df586818b56bce6c495e90bcae11c5145ac3e1
David Krueger <davidscottkrueger@gmail.com>
2016-04-11 17:49:20 -0400

    make defaults print|

--- parser_fail.py
+++ parser_fail.py
@@ -1,7 +1,7 @@
 
 import argparse
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("--test", type=int, dest='test', default=1)
+parser.add_argument("--test", type=int, dest='test', help="WHY NO DEFAULT???", default=1)
 parser.parse_args()
 args_dict = vars(parser.parse_args())
 locals().update(args_dict)
